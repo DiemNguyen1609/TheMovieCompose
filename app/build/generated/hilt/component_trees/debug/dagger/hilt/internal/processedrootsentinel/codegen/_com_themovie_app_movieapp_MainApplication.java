2813cdea753bd722dfa1a7a97db89efb7ffd6c51
Nia (Diem Nguyen) <diemn@joblogic.com>
2025-05-23 16:51:11 +0700

    fix: remove unused file

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_themovie_app_movieapp_MainApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.themovie.app.movieapp.MainApplication"
-)
-public final class _com_themovie_app_movieapp_MainApplication {
-}
--- app/build/tmp/kapt3/stubs/debug/com/themovie/app/movieapp/presentation/ui/tasks/TasksViewModelKt.java
+++
@@ -1,7 +0,0 @@
-package com.themovie.app.movieapp.presentation.ui.tasks;
-
-@kotlin.Metadata(mv = {1, 9, 0}, k = 2, xi = 48, d1 = {"\u0000\b\n\u0000\n\u0002\u0010\u000e\n\u0000\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0002"}, d2 = {"TASKS_FILTER_SAVED_STATE_KEY", "", "app_debug"})
-public final class TasksViewModelKt {
-    @org.jetbrains.annotations.NotNull()
-    public static final java.lang.String TASKS_FILTER_SAVED_STATE_KEY = "TASKS_FILTER_SAVED_STATE_KEY";
-}
